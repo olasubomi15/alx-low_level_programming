09c57f883cbdde01e37a98ce30d398721a23ecb4
olasubomi15 <olufadejiolasubomi15@gmail.com>
2022-12-10 14:15:23 -0800

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
