fd219030aae7d576b35edec0fbcf1d3dc0ffe0bc
olasubomi15 <olufadejiolasubomi15@gmail.com>
2023-01-18 04:27:12 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: pointer to variable name
+ * @f: pointer to a function f that takes a pointer to a
+ * char as an argument and returns void
+ * Return: Void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+
+}
